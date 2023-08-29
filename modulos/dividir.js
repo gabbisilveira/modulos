b4fdfcc2c104bc19a530c151eaf472e252d66e43
gabriela <gabbisilveiraaa@gmail.com>
2023-08-29 15:38:42 -0300

    calculadora completa

---
+++ modulos/dividir.js
@@ -0,0 +1,4 @@
+var dividir = function (x,y) {
+    return (x / y) / 2;
+  }; 
+  module.exports = dividir; 
---
+++ modulos/subtrair.js
@@ -0,0 +1,4 @@
+var subtrair = function (x,y) {
+    return (x - y) / 2;
+  }; 
+  module.exports = subtrair; 
